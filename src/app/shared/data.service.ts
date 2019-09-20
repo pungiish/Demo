75a89f0a46814f9a65e854a9dd4dbcd2be32c926
jan pungarsek <jan.pungarsek@gmail.com>
2019-09-20 16:05:54 +0200

    Moved api to environment variable.

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
-import { ConfigService } from 'src/assets/config'
-import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { environment as env } from 'src/environments/environment'
+import { Observable } from 'rxjs';
 
-import { tap, map, catchError } from 'rxjs/operators'
 import { User } from '../models/User';
 @Injectable({
 	providedIn: 'root'
@@ -17,17 +16,17 @@ export class DataService {
 			'Content-Type': 'application/json',
 		})
 	};
-	constructor (private http: HttpClient, private config: ConfigService) {
+	constructor (private http: HttpClient) {
 	}
 
 	create (user: User): Observable<any> {
-		return this.http.post(this.config.registerUrl, user, this.httpOptions)
+		return this.http.post(env.req + '/register', user, this.httpOptions)
 	}
 	read (): Observable<any> {
 		this.httpOptions.headers.append('token', this.token);
-		return this.http.get<any>(this.config.usersUrl, this.httpOptions);
+		return this.http.get<any>(env.req + '/users', this.httpOptions);
 	}
 	logIn (user: User): Observable<string> {
-		return this.http.post<string>(this.config.loginUrl, user, this.httpOptions)
+		return this.http.post<string>(env.req + '/login', user, this.httpOptions)
 	}
 }
